a781ee9f23793b1e71880f806fe810b0c12dc061
Ten <Ten@pal-nat186-80-108.itap.purdue.edu>
2019-06-08 17:09:32 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+/*
+heroku login
+heroku create
+git remote add heroku https://...
+git push heroku master
+*/
