0fc4105f6357fc4b9dd79cc3076251578c75e8cd
Ten <Ten@campus-085-122.ucdavis.edu>
2019-06-13 19:51:58 -0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./mondels/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
@@ -14,3 +30,6 @@ heroku create
 git remote add heroku https://...
 git push heroku master
 */
+
+// https://accounts.google.com/o/oauth2/v2/auth?
+// response_type = code & scope=profile % 20email & client_id=P_kgr7JhwfXKOdnNkfaCxMDi
